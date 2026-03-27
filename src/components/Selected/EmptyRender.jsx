88ac2d7040ba7fb7537dffb7a14c4d49cfcf90da
yousufrahman <ryousuf766@gmail.com>
2026-03-27 12:40:46 +0600

    added the empthy render

--- src/components/Selected/EmptyRender.jsx
+++ src/components/Selected/EmptyRender.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const EmptyRender = () => {
     return (
         <>
-        empty
+        <div className='flex justify-center'>
+            <h2 className='text-center font-bold text-4xl'>Please Select your favourite Player</h2>
+        </div>
         </>
     );
 };
